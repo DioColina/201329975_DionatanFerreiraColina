9d56d1b7d209c389633122543263fc3307bde572
Siegweiss <Dionathamcolina@gmail.com>
2021-08-26 14:04:26 -0300

    Header e Footer

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Docil - Doces Artesanais</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&display=swap" rel="stylesheet">
+
+<style>
+    body {
+     margin: 0;
+     font-family: 'Poppins', sans-serif;   
+    }
+
+    header {
+        height: 100px;
+        background-color: #a94b00;
+        text-align: center;
+        padding: 10px;
+    }
+
+    .logo {
+        height: 80px;
+    }
+
+    nav {
+        background-color: #c08457;
+    }
+
+    nav ul{
+        list-style: none;
+        width: 100%;
+        margin: 0px;
+        padding: 0px;
+        display: flex;
+        justify-content: center;
+    }
+
+    nav ul li{
+        padding-top: 12px;
+        padding-bottom: 12px;
+        padding-left: 23px;
+        padding-right: 23px;
+    }
+
+    .content{
+        min-height: 450px;
+    }
+
+    footer{
+        background-color: #c08457;
+        min-height: 55px;
+        padding: 40px;
+    }
+
+</style>
+
+</head>
+<body>
+  
+    <header>
+        <img src="mapa/imagens/LOGO BLACK.png" alt="Logo" class="logo">
+    </header>
+
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>Quem Somos</li>
+            <li>Contato</li>
+            <li>Localização</li>
+        </ul>
+    </nav>
+ 
+    <div class="content">
+        conteúdo
+    </div>
+
+    <footer>
+        RA: 201329975 / Nome: Dionatan Ferreira Colina
+    </footer>
+</body>
+</html>
